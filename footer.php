a4592f88429f24b0aa5d1ff5c5f07f9d119a81ee
miguel.vidal <miguel.vidal@gobierna.com.mx>
2016-07-31 17:17:01 -0500

    recordadno

---
+++ footer.php
@@ -0,0 +1,130 @@
+			<!-- footer -->
+			<footer class="footer" role="contentinfo" >
+
+		
+					 <div class="container">
+					 		<div class="row">
+						 		<div class="col-sm-4"> 
+						 			<a href="<?php echo home_url(); ?>">
+									  <img src="<?php echo get_template_directory_uri(); ?>/img/logo2.png" alt="Logo" class="logo-img">
+									  </a>
+						 		</div>
+								<div class="col-sm-2">Radio Alarma</div>
+								<div class="col-sm-2">Paquetes</div>
+								<div class="col-sm-4">Artigulos de interes</div>
+					 		</div><!-- row -->
+					 		<div class="row">
+					 			<div class="col-sm-4">
+					 				<div class="row">
+					 				Lorem ipsum dolor sit amet, consectur adiplscing elit. Nam
+					                vivera eusimod odio, gravida pellentesque unma varius vitaem sed.
+					                Dui lorem , adisplicing in adisplcing et, intercedum nec.
+					 				</div>
+					 				<div class="row">
+					 					<div class="row">
+						 					<div class="col-sm-2">
+						 					<small>Phone:</small> </div>
+						 					<div class="col-sm-9">
+						 					<small>(999) 81-40/(999)81-40-86</small> </div>
+					 					</div>
+					 					<div class="row">
+						 					<div class="col-sm-1">
+						 					<small>email:</small> </div>
+						 					<div class="col-sm-1">
+						 					<ul>
+							                  <li><small>ventas@radioalarma.com</small> </li>
+							                  <li><small>ventas@radioalarma.com</small> </li>
+							                  <li><small>ventas@radioalarma.com</small> </li>
+							                </ul>
+						 					</div>
+					 					</div>
+					 				</div>
+					 			
+					 			</div>
+					 			<div class="col-sm-2">
+					 				<ul>
+					                  <li>Inicio</li>
+					                  <li>Inicio</li>
+					                  <li>Inicio</li>
+					                  <li>Inicio</li>
+					                  <li>Inicio</li>
+					                  <li>Inicio</li>
+					                  <li>Inicio</li>
+					                  <li>Inicio</li>
+					                </ul>
+					 			</div><!-- fin del colsm3deinicio -->
+					 			<div class="col-sm-2">
+					 				<ul>
+					                  <li>Casa</li>
+					                  <li>Oficina</li>
+					                  <li>Comercio</li>
+					                  <li>Fabrica</li>
+					                </ul>
+					 			</div><!-- fin del colsm3deinicio2 -->
+					 			<div class="col-sm-4">
+					 					<div class="row">
+					 						<div class="col-sm-6">
+					 								<img src="<?php echo get_template_directory_uri(); ?>/img/logo2.png" alt="imag1" class="img-rounded">
+					 						</div>
+					 						<div class="col-sm-6">
+					 								<small>lorem ipsum dolor sit amet, consctur adisplcing elit</small>
+					 						</div>
+					 						<div class="col-sm-1">
+					 								<small>26/sept/1992</small>
+					 						</div>
+					 					</div>
+					 					<div class="row">
+					 							<div class="col-sm-6">
+					 									<img src="<?php echo get_template_directory_uri(); ?>/img/logo2.png" alt="imag1" class="img-rounded">
+					 							</div>
+					 							<div class="col-sm-6">
+					 								<small>lorem ipsum dolor sit amet, consctur adisplcing elit</small>
+					 							</div>
+					 							<div class="col-sm-1">
+					 								<small>26/sept/199123122</small>
+					 							</div>
+					 					</div>
+									
+					                  
+					                  
+					               
+					 			</div>
+					 			
+					 		</div><!-- row -->
+					 	
+					 </div><!-- container -->
+
+					<div class="bannerbot">
+					<br>
+						<div class="row">
+							<div class="col-sm-4"> </div>
+							<div class="col-sm-4">Aviso de Privacidad </div>
+							<div class="col-sm-4">Copyright 2014 Radio Alarmas</div>
+
+							
+						</div>
+					</div>
+
+
+				<!-- /copyright -->
+
+			</footer>
+			<!-- /footer -->
+
+		</div>
+		<!-- /wrapper -->
+
+		<?php wp_footer(); ?>
+
+		<!-- analytics -->
+		<script>
+		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+		ga('send', 'pageview');
+		</script>
+
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="<?php bloginfo('description'); ?>">
+
+		<?php wp_head(); ?>
+		<script>
+        // conditionizr.com
+        // configure environment tests
+        conditionizr.config({
+            assets: '<?php echo get_template_directory_uri(); ?>',
+            tests: {}
+        });
+        </script>
+
+	</head>
+	<body <?php body_class(); ?>>
+
+		<!-- wrapper -->
+		<div class="wrapper">
+
+			<!-- header -->
+			<header class="header clear" role="banner">
+					<!-- BannerTOP -->
+
+					<div class="bannertop">
+					<br>
+						<div class="row">
+							<div class="col-sm-1"> </div>
+							<div class="col-sm-2">ventas@radioalarmas.com.mx</div>
+							<div class="col-sm-2">direccion@radioalarmas.com.mx</div>
+							<div class="col-sm-3">ventas@radioalarmas@hotmail.com</div>
+							<div class="col-sm-3">(999) 981-40-85 y 981-40-86</div>
+							<div class="col-sm-1">logo face</div>
+						</div>
+					</div>
+					<!-- logo -->
+				
+					<!-- /logo -->
+
+					<!-- nav -->
+				<!-- 	<nav class="nav" role="navigation">
+						<?php html5blank_nav(); ?>
+					</nav> -->
+
+					<div class="container">
+							<div class="row">
+								<div class="col-sm-3"> 
+									<div class="logo">
+									  <a href="<?php echo home_url(); ?>">
+									  <img src="<?php echo get_template_directory_uri(); ?>/img/logo2.png" alt="Logo" class="logo-img">
+									  </a>
+									</div>
+								</div>
+								<div class="col-sm-7"> 
+										<div class="row">
+											<nav class="navbar navbar-default">
+												<div class="container">
+													<div class="navbar-header">
+														<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+															<span class="sr-only">Toggle navigation</span>
+															<span class="icon-bar"></span>
+															<span class="icon-bar"></span>
+															<span class="icon-bar"></span>
+														</button>
+													</div>
+													 <div id="navbar" class="navbar-collapse collapse">
+														<?php html5blank_nav(); ?>
+													</div><!--/.nav-collapse -->
+												</div>
+											</nav>
+										</div>
+										<div class="row">
+											  <nav class="navbar navbar-default">
+										            <div class="container">
+										              <div class="navbar-header">
+										                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+										                  <span class="sr-only">Toggle navigation</span>
+										                  <span class="icon-bar"></span>
+										                  <span class="icon-bar"></span>
+										                  <span class="icon-bar"></span>
+										                </button>
+										              </div>
+										               <div id="navbar" class="navbar-collapse collapse">
+										                <?php html5blank_nav2(); ?>
+										              </div><!--/.nav-collapse -->
+										            </div>
+										          </nav>
+										</div>
+										
+								</div>
+										<!-- end of col-sm -8 -->
+							</div><!-- end of row-->
+				
+					</div>
+					 
+					
+					<!-- second nav-->
+
+					
+					<!-- /nav -->
+
+			</header>
+			<!-- /header -->
