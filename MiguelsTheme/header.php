6de24b10f8b3a2a8e8d0ab983225177344cb94f1
Daniel Valenzuela <daniel.valenzuela@4thsource.com>
2016-08-02 16:23:43 -0500

    Fixed nav in mobile

--- MiguelsTheme/header.php
+++ MiguelsTheme/header.php
@@ -83,14 +83,14 @@
 											  <nav class="navbar navbar-default">
 										            <div class="container">
 										              <div class="navbar-header">
-										                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+										                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar2" aria-expanded="false" aria-controls="navbar">
 										                  <span class="sr-only">Toggle navigation</span>
 										                  <span class="icon-bar"></span>
 										                  <span class="icon-bar"></span>
 										                  <span class="icon-bar"></span>
 										                </button>
 										              </div>
-										               <div id="navbar" class="navbar-collapse collapse">
+										               <div id="navbar2" class="navbar-collapse collapse">
 										                <?php html5blank_nav2(); ?>
 										              </div><!--/.nav-collapse -->
 										            </div>
