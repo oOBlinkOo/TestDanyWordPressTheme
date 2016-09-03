d9ab2610f00be90bb6ab89cf91eb1c672eca4bff
miguel.vidal <miguel.vidal@gobierna.com.mx>
2016-09-03 13:08:06 -0500

    ya mero

--- MiguelsTheme/productos_template.php
+++ MiguelsTheme/productos_template.php
@@ -101,6 +101,92 @@
               </ul>
 
 			<?php endif; ?>	
+
+
+<?php
+		$nombre1 = get_field('nombre1');
+		$precio1 = get_field('precio1');	
+		$imgventa1 = get_field('imgventa1');
+		$descripcion1 = get_field('descripcion1');
+
+		$nombre2 = get_field('nombre2');
+		$precio2 = get_field('precio2');	
+		$imgventa2 = get_field('imgventa2');
+		$descripcion2 = get_field('descripcion2');
+?>
+
+			<div class="col-xs-6 col-md-9">
+			<div  id="carousel-example-generic"class="carousel slide" data-ride="carousel">
+				<div class="row ">
+					 <div class="carousel-inner" role="listbox">
+					 	   <div class="item active">
+							        <div class="col-md-6">
+										<div class="row componenteProductoEstandart">
+											<div class="row name"><?php echo $nombre1; ?></div>
+											<div class="row description"><?php echo $descripcion1; ?></div>
+											<div class="row price"><?php echo $precio1; ?></div>
+										</div>
+
+									</div>	
+									<div class="col-md-3">
+										 <img class="img-thumbnail" src="<?php echo $imgventa1['url']; ?>">
+									</div>	
+					      </div>
+					         <div class="item">
+							        <div class="col-md-6">
+										<div class="row componenteProductoEstandart">
+											<div class="row name"><?php echo $nombre2; ?></div>
+											<div class="row description"><?php echo $descripcion2; ?></div>
+											<div class="row price"><?php echo $precio2; ?></div>
+										</div>
+
+									</div>	
+									<div class="col-md-3">
+										 <img class="img-thumbnail" src="<?php echo $imgventa2['url']; ?>">
+									</div>	
+					      </div>
+
+
+
+					 </div>
+
+
+					
+
+
+
+				</div>
+				  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+				    <span class="icon-prev" aria-hidden="true"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+				    <span class="icon-next" aria-hidden="true"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
+			</div>
+			</div>
+<!-- 
+  <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="img_chania.jpg" alt="Chania" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="img_chania2.jpg" alt="Chania" width="460" height="345">
+      </div>
+    
+      <div class="item">
+        <img src="img_flower.jpg" alt="Flower" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="img_flower2.jpg" alt="Flower" width="460" height="345">
+      </div>
+    </div>
+ -->
+
+
 			<div class="col-xs-6 col-md-3">
 				
 
@@ -162,16 +248,20 @@
 		            $precio = get_sub_field('precio');
             		// echo "paso" + var_dump($categoria);
             	?>
-            	
-            	<div class="bg-warning col-xs-6 col-md-3 data-<?php echo str_replace(' ','-',$categoria);?> productborder " >
+         
+            			<div class="bg-warning col-xs-6 col-md-3 data-<?php echo str_replace(' ','-',$categoria);?> productborder " >
             	<!-- <div class="thumbnail"> -->
             		<div class="col-md-6 bg-success" ><div class="row"><p class="nombre"> <?php echo "$nombre";?></p></div>
-            			<div class="row"> <img src="<?php echo $image['url']; ?>" alt="Chania"></div>
+            			<div class="row"> <img class="img-thumbnail productImage" src="<?php echo $image['url']; ?>" alt="Chania"></div>
             		</div>
             		<div class="col-md-6   descripcion"><p class=""> <?php echo "$description";?></p></div>
             	<!-- </div> -->
+            	<br>
 				</div>
 
+
+            	
+            
 
              	<?php endwhile; ?>
 			<?php endif; ?>	
